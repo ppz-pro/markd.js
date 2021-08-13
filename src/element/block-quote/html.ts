3ebaa73ee772607db33bec4a103b24f4abebb80b
gy <372301467@qq.com>
2021-08-13 14:38:22 +0800

    合并多行 blockquote

--- src/element/block-quote/html.ts
+++ src/element/block-quote/html.ts
@@ -1,8 +1,8 @@
-import Abstract from '../base/html'
+import Abstract from '../base/html/children'
 
 export default
 class BlockQuoteElement extends Abstract {
-  constructor(content: string) {
-    super('blockquote', content)
+  constructor() {
+    super('blockquote')
   }
 }
--- src/element/block-quote/parser.ts
+++ src/element/block-quote/parser.ts
@@ -1,11 +1,27 @@
 import Parser from '../base/parser/simple'
 import Element from './html'
+import PElement from '../p/html'
 
 export default
 class HParser extends Parser<Element> {
   protected regExp = /^> (.+)/
+
   protected parse(result: RegExpExecArray) {
-    this.readline.next()
-    return new Element(result[1])
+    const bq = new Element()
+    bq.push(new PElement(result[1]))
+    return this.parseMore(bq)
+  }
+
+  protected parseMore(bq: Element): Element {
+    const next = this.readline.next()
+    
+    if(next == undefined)
+      return bq
+
+    const nextResult = this.regExp.exec(next)
+    if(nextResult)
+      return this.parseMore(bq)
+    else
+      return bq
   }
 }
