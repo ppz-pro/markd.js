e902a6170f36fb23e8a420d87eb62774b77b4d85
gy <372301467@qq.com>
2021-08-13 17:06:18 +0800

    stage

--- example/index.js
+++ example/index.js
@@ -1,8 +1,8 @@
-const Path = require('path')
-const FS = require('fs')
+import Path from 'path'
+import FS from 'fs'
 
-const Readline = require('@ppzp/ppz/readline/cached-file.js')
-const DocParser = require('@ppzp/ppz/parser.js')
+import Readline from '@ppzp/ppz/readline/cached-file.js'
+import DocParser from '@ppzp/ppz/parser.js'
 
 ;(async function() {
   const input = Path.join(__dirname, '../readme-input.md')
@@ -11,6 +11,6 @@ const DocParser = require('@ppzp/ppz/parser.js')
   const readline = new Readline(input)
   await readline.readPromise
 
-  const parser = new DocParser()
+  const parser = new DocParser(readline)
   FS.writeFileSync(output, parser.parse().toHTML())
 })()
